19277b7bea793605db4284345da72510ff8f06b7
MoZgA_SaVe <mozg9637@gmail.com>
2026-08-08 17:15:17 +0300

    Refactor patient appointment model and add details

--- PatientPlanner/Models/Models.swift
+++ PatientPlanner/Models/Models.swift
@@ -1,19 +1,7 @@
 import Foundation
 
-// Модель данных пациента и осмотра
-struct PatientAppointment: Identifiable, Codable {
-    var id: UUID = UUID()
-    
-    // Шапка / Основное
-    var patientName: String
-    var patientNumber: String
-    var birthDate: String
-    var inspectionDate: Date = Date()
-    var inspectionType: String = "Взрослая"
-    var doctorName: String = ""
-    var nurseName: String = ""
-    
-    // Анамнез и жалобы
+// Дополнительные данные стоматолога
+struct StomatologyDetails: Codable {
     var complaints: String = ""
     var allergicAnamnesis: String = ""
     var diseaseAnamnesis: String = ""
@@ -22,32 +10,31 @@ struct PatientAppointment: Identifiable, Codable {
     var objectively: String = ""
     var bite: String = ""
     var xRayResults: String = ""
-    
-    // Статусы и диагноз
     var mucousMembrane: String = "Норма"
     var isSanationCompleted: Bool = false
     var requiresProsthetics: Bool = false
     var icd10Diagnosis: String = ""
-    
-    // Лечение
     var treatmentPlan: String = ""
     var anesthesia: String = ""
     var treatment: String = ""
     var prescriptions: String = ""
     var generalRecommendations: String = ""
     
-    // Листок нетрудоспособности (ЛН)
+    // ЛН
     var isSickLeaveIssued: Bool = false
     var sickLeaveStartDate: Date = Date()
     var sickLeaveEndDate: Date = Date()
-    var sickLeaveClosedDate: Date? = nil
     var sickLeaveNumber: String = ""
-    
-    var extendedProlongations: [SickLeaveExtension] = []
 }
 
-struct SickLeaveExtension: Identifiable, Codable {
+// Дополняем существующую структуру Appointment (или аналогичную из вашего проекта)
+struct Appointment: Identifiable, Codable {
     var id: UUID = UUID()
-    var startDate: Date
-    var endDate: Date
+    // СТАРЫЙ ФУНКЦИОНАЛ (сохраняем)
+    var patientName: String
+    var doctorName: String
+    var date: Date
+    
+    // НОВЫЙ ФУНКЦИОНАЛ (добавляем)
+    var stomatologyDetails: StomatologyDetails? 
 }
